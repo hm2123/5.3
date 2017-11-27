b103d51ddb5d50eea151a4ac86b02b53d3c77c01
hm2123 <31521331+hm2123@users.noreply.github.com>
2017-11-27 03:18:43 -0500

    Update CarDriver.java

--- src/CarDriver.java
+++ src/CarDriver.java
@@ -12,7 +12,6 @@ public class CarDriver {
         System.out.println(hisCar);
         System.out.println(herCar);
 
-
         myCar.setColor("blue");
         hisCar.setColor("blue");
         herCar.setColor("blue");
@@ -24,13 +23,12 @@ public class CarDriver {
         myCar.setHorsepower(650);
         hisCar.setHorsepower(650);
         herCar.setHorsepower(650);
-
+        
         myCar.setEngineSize(6.5);
         hisCar.setEngineSize(6.5);
         herCar.setEngineSize(6.5);
-
-
-        System.out.println("\n(All cars have been changed to Orange Aston Martin's with 700HP and 8.5 Engine Size)");
+        
+        System.out.println("\n(All cars have been changed to blue Ferrari's with 650HP and 6.5 Engine Size)");
         System.out.println("myCar: " + myCar.getColor() + " " + myCar.getHorsepower() + " " +  myCar.getEngineSize() + " " + myCar.getMake());
         System.out.println("hisCar: " + hisCar.getColor() + " " + hisCar.getHorsepower() + " " +  hisCar.getEngineSize() + " " + hisCar.getMake());
         System.out.println("herCar: " + herCar.getColor() + " " + herCar.getHorsepower() + " " +  herCar.getEngineSize() + " " + herCar.getMake());
